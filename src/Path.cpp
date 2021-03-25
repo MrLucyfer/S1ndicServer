9212b56f98b863fc810fcc5d2f23b83dea93c54f
Nil Folquer <nfolquer@gmail.com>
2021-03-25 18:48:38 +0100

    First approach of the Path class. Now it tells you if is either a File or a Directory in order to read the file or send a 404

--- src/Path.cpp
+++ src/Path.cpp
@@ -1,14 +1,37 @@
 #include "Path.h"
 #include "Logger.cpp"
 #include <filesystem>
+#include <fstream>
+#include <vector>
 
 Path::Path() {
     std::filesystem::path temp = std::filesystem::current_path();
     m_rootPath = temp.string();
     m_rootPath += "/public_html";
-    Logger::PrintMessage(m_rootPath);
+    //Logger::PrintMessage(m_rootPath);
 }
 
 Path::~Path() {
 
 }
+
+char* Path::Open(const string& rel_path) {
+    if(IsDirectory(rel_path)) {
+        return nullptr;
+    } else {
+        std::fstream file(rel_path);
+        std::ifstream::pos_type pos = file.tellg();
+        std::vector<char> bytes(pos);
+
+        file.seekg(0, ios::beg);
+        file.read(&bytes[0], pos);
+
+        return bytes.data();
+    }
+}
+
+bool Path::IsDirectory(const string& rel_path) {
+    std::filesystem::path temp_path(rel_path);
+    return std::filesystem::is_directory(temp_path);
+}
+
--- src/Path.h
+++ src/Path.h
@@ -5,6 +5,12 @@ class Path {
 public:
     Path();
     ~Path();
+
+    char* Open(const std::string& rel_path);
+
+private:
+    bool IsDirectory(const std::string& rel_path);
+
 private:
     std::string m_rootPath;
 };
